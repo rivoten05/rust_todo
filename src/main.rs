f2254d75f224284f0f30283ae9326abb5663b353
hitachi-payments <hitachi-payments@gmail.com>
2025-12-31 10:32:26 +0530

    logs

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use sqlx::{Executor, SqlitePool, prelude::FromRow};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+    // $env:RUST_LOG="info"; cargo watch -c -x r
     let pool = db().await;
     HttpServer::new(move || {
         App::new()
